355424ba50aa398a13e7dfa25a5faf91c5ba54ec
avavaaya <nana7777777mi@gmail.com>
2024-08-09 17:33:45 +0800

    fix links

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -30,14 +30,22 @@ export const getQuote = async () => {
 };
 
 export const getMembers = async (): Promise<string> => {
+
+    // name . xxx . <u><a class="text-light-blue dark:text-dark-blue underline" href="" target="_blank"></a></u>
   return `  Active Members
----
-0xd009 . reverse . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://0xd009.github.io/" target="_blank">https://0xd009.github.io/</a></u>
-brealid . crypto . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://www.brealid.cn/" target="_blank">https://www.brealid.cn/</a></u>
-eastXueLian . pwn . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://eastxuelian.nebuu.la/" target="_blank">https://eastxuelian.nebuu.la/</a></u>
-crabtux . full-stack . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://crabtux.github.io/" target="_blank">https://crabtux.github.io/</a></u>
-wuyuewy . web . <u><a class="text-light-blue dark:text-dark-blue underline" href="http://home.ustc.edu.cn/~wuyuewy/" target="_blank">http://home.ustc.edu.cn/~wuyuewy/</a></u>
-AlbedoW . misc . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://albedowang.github.io/" target="_blank">https://albedowang.github.io/</a></u>`;
+
+    0xd009 . reverse . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://0xd009.github.io/" target="_blank">https://0xd009.github.io/</a></u>
+    brealid . crypto . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://www.brealid.cn/" target="_blank">https://www.brealid.cn/</a></u>
+    eastXueLian . pwn . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://eastxuelian.nebuu.la/" target="_blank">https://eastxuelian.nebuu.la/</a></u>
+    crabtux . full-stack . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://crabtux.github.io/" target="_blank">https://crabtux.github.io/</a></u>
+    wuyuewy . web . <u><a class="text-light-blue dark:text-dark-blue underline" href="http://home.ustc.edu.cn/~wuyuewy/" target="_blank">http://home.ustc.edu.cn/~wuyuewy/</a></u>
+    AlbedoW . misc . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://albedowang.github.io/" target="_blank">https://albedowang.github.io/</a></u>
+-----------------------
+  Honorary Members
+
+    ichild . reverse & pwn . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://github.com/" target="_blank">hidden</a></u>
+    hklst4r . blockchain & crypto . <u><a class="text-light-blue dark:text-dark-blue underline" href="https://github.com/" target="_blank">hidden</a></u>
+`;
 };
 
 export const getAwards = async (): Promise<string> => {
